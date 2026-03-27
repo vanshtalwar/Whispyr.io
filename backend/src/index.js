f0e41d8118dd703fd0ccc3db96fe0b053f65c7f9
Vansh Talwar <vipultalwin99@gmail.com>
2026-03-27 14:27:21 +0530

    push

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,10 +10,8 @@ import cors from "cors";
 const app = express()
 app.use(
   cors({
-    origin: "https://whispyr-io.vercel.app/", // your frontend URL
-    credentials: true, // allow cookies/auth headers
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"], // ✅ this line fixes your current error
+    origin: ["https://whispyr-io.vercel.app"],
+    credentials: true,
   })
 );
 app.use(express.json())
