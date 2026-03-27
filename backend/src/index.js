50b5fd1465cf8df7142cfc865b26d51540735cbe
Vansh Talwar <vipultalwin99@gmail.com>
2026-03-27 14:00:46 +0530

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,8 @@
 import express from "express"
-import authRoutes from './src/routes/auth.route.js'
-import messageRoutes from './src/routes/message.route.js'
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import dotenv from "dotenv"
-import {connectDB}  from "./src/lib/db.js"
+import {connectDB}  from "./lib/db.js"
 import cookieParser from "cookie-parser"
 import cors from "cors";
 
